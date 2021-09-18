6edfa440a5d75ba0afa52d495f8a0c24888ca643
SBSstudio <86954238+SBSstudio@users.noreply.github.com>
2021-09-18 10:31:32 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,32 @@ import png
 from pyqrcode import QRCode
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 from telegram import Update
+import ffmpeg
+import uuid
+import math
+import asyncio
+import logging
+import threading
+import unittest, time, datetime
+import urllib.request, urllib.error, urllib.parse
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import InvalidArgumentException
+from selenium.webdriver.chrome.options import Options
+from pyrogram import Client, filters
+from asyncio import get_running_loop
+from functools import partial
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+import pyrogram
+from pyrogram import Client, filters
+from pyrogram.errors import MessageNotModified
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+import shutil
+
 TOKEN = os.environ.get('TOKEN')
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
@@ -52,7 +78,41 @@ def msg(update: Update, context: CallbackContext) -> None:
     except Exception:
         update.message.reply_text("Please Try Agian Later")
 
-
+        
+async def pyro_fsub(c, message, fsub):
+    try:
+        user = await c.get_chat_member(fsub, message.chat.id)
+        if user.status == "kicked":
+            await c.send_message(
+                chat_id=message.chat.id,
+                text="Sorry, You are Banned to use me.",
+                parse_mode="markdown",
+                disable_web_page_preview=True
+            )
+        return True
+    except UserNotParticipant:
+        chnl = os.environ.get("UPDTE_CHNL")
+        await c.send_message(
+            chat_id=message.chat.id,
+            text="**Please Join My Updates Channel to Use Me!**",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("Join Now", url=f"https://t.me/{chnl}")
+                    ]
+                ]
+            )
+        )
+        return False
+    except Exception as kk:
+        print(kk)
+        await c.send_message(
+            chat_id=message.chat.id,
+            text="Something went Wrong.",
+            parse_mode="markdown",
+            disable_web_page_preview=True)
+        return False
+    
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
